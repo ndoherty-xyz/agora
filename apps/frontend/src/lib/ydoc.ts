92dd896d9b4d9bf50804b329d73b2e839339bf38
ndoherty <ndoherty.xyz@gmail.com>
2025-12-11 19:24:28 -0700

    fix env vars

--- apps/frontend/src/lib/ydoc.ts
+++ apps/frontend/src/lib/ydoc.ts
@@ -1,7 +1,8 @@
 import * as Y from "yjs";
 import { HocuspocusProvider } from "@hocuspocus/provider";
 
-export const SERVER_DOMAIN = process.env.SERVER_DOMAIN ?? "localhost:3001";
+export const SERVER_DOMAIN =
+  process.env.NEXT_PUBLIC_SERVER_DOMAIN ?? "localhost:3001";
 
 export const ydoc = new Y.Doc();
 
